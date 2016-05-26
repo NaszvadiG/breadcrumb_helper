f1bfc5bd197a0a34f4169844373833f4f29bf01c
Anthony M <anthony@amorrissound.com>
2016-05-26 08:37:20 -0700

    Update breadcrumb_helper.php
    Added the ability to manually set an HTTP referer.

--- breadcrumb_helper.php
+++ breadcrumb_helper.php
@@ -3,18 +3,28 @@
 	function set_breadcrumb()
     {
 		$CI =& get_instance();
-		$session_data = $CI->session->userdata('logged_in');
-        if(isset($_SERVER['HTTP_REFERER']))
+		$manual_referer = $CI->session->userdata('manual_referer');
+		
+        if((isset($_SERVER['HTTP_REFERER'])) AND ($manual_referer == ''))
         {
 			$CI->session->set_userdata('last_breadcrumb', $_SERVER['HTTP_REFERER']);
-        }
-        else
-        {
+        } else if ($manual_referer != '') {
+			$CI->session->set_userdata('last_breadcrumb', $manual_referer);
+			$CI->session->unset_userdata('manual_referer');
+		} else {
 			$CI->session->set_userdata('last_breadcrumb', $_SERVER['SERVER_NAME']);
         }
-		
+	
 		Return;
     }
+	
+	function manual_referer($referer) 
+	{
+		$CI =& get_instance();
+		$CI->session->set_userdata('manual_referer', $referer);
+	
+		Return;
+	}
 	
 	function last_breadcrumb()
 	{
